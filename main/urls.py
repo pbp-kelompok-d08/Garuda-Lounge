07f4f9f30c2f3bbf59ca330a9453da70f40ab1f6
lleopie306 <desver4@gmail.com>
2025-10-22 22:36:22 +0700

    Menyelesaikan fitur login logout dan membuat landing page main berserta dengan navbar nya

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import show_main, login_user, logout_user, register_user
+from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('login/', login_user, name='login_user'),
-    path('logout/', logout_user, name='logout_user'),
-    path('register/', register_user, name='register_user'),
+    path('', views.show_main, name='show_main'),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('register/', views.register_user, name='register_user'),
+    path('landingpage/', views.show_landingpage, name='show_landingpage'),
+    path('json/', views.show_json, name='show_json'),
+    path('xml/', views.show_xml, name='show_xml'),
 ]
