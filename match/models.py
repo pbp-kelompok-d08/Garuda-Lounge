e11bca5787134511bc49080ffa1595be249b29f5
farrelzzz <rajendramuhammadfarrel@gmail.com>
2025-10-22 14:36:15 +0700

    buat model app match

--- match/models.py
+++ match/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Match(models.Model):
+    CATEGORY_CHOICES = [
+        ('kualifikasi piala dunia', 'Kualifikasi Piala Dunia'),
+        ('pertandingan persahabatan', 'Pertandingan Persahabatan'),
+        ('piala aff', 'Piala AFF'),
+        ('piala asia', 'Match'),
+    ]
---
+++ match/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from garuda_lounge.urls import urlpatterns
+
+app_name = 'main'
+
+urlpatterns = [
+    urlpatterns,
+    #path('', show_main, name='show_main'),
+]
