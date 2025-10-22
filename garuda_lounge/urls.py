b9ec6f9bac1c0b404fa631097b432d97f62ee501
Dzidx <dzaky.trinindito@gmail.com>
2025-10-22 16:44:06 +0700

    changed urls.py in garuda_lounge

--- garuda_lounge/urls.py
+++ garuda_lounge/urls.py
@@ -19,5 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('merchandise.urls', namespace='merchandise')),
+    path('', include('main.urls', namespace='main')),
+    path('match/', include('match.urls', namespace='match')),
+    path('merchandise/', include('merchandise.urls', namespace='merchandise')),
+    path('news/', include('news.urls', namespace='news')),
+    path('ProfileAktif/', include('ProfileAktif.urls', namespace='ProfileAktif')),
+    path('ProfileLegend/', include('ProfileLegend.urls', namespace='ProfileLegend')),
 ]
