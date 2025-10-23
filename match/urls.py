01e4c5d5da5939113b6482c81e7e4a07f54d96e2
farrelzzz <rajendramuhammadfarrel@gmail.com>
2025-10-24 00:46:15 +0700

    nambahin form dan card buat pertandingan. fitur nambahin pertandingan dah oke, tapi styling ga tau kenapa belum applied. card dah oke, tinggal bikin tombol buat liat detail pertandingan sama mungkin highlightnya dipindahin ke halaman detail pertandingan aja. sama tampilan match.html nya kok malah ngawur dah

--- match/urls.py
+++ match/urls.py
@@ -5,10 +5,11 @@
 #urlpatterns = [ ...
 
 from django.urls import path
-from match.views import show_match, add_match
+from match.views import show_match, add_match, show_match_detail
 
 app_name = 'match'
 urlpatterns = [
     path('', show_match, name='show_match'),
     path('add-match/', add_match, name='add_match'),
+    path('<str:id>', show_match_detail, name='show_match_detail')
 ]
--- match/views.py
+++ match/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from match.forms import PertandinganForm
+from match.models import Pertandingan
 
 # Create your views here.
 def show_match(request):
-    return render(request, "match.html")
+    matches = Pertandingan.objects.all()
+    context = {
+        'pertandingan_list': matches # <-- BENAR! Namanya cocok.
+    }
+    return render(request, "match.html", context)
 
 def add_match(request):
-    return render(request, "add_match.html")
+    form = PertandinganForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('match:show_match')
+
+    context = {'form': form}
+    return render(request, "add_match.html", context)
+
+def show_match_detail(request, id):
+    pertandingan = get_object_or_404(Pertandingan, pk=id)
+
+    context = {
+        'pertandingan': pertandingan
+    }
+
+    return render(request, "match_detail.html", context)
